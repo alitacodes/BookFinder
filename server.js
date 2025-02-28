07381f0dc527b3b2847dbcd232cd26b8714ca08a
alitacodes <techalita7070@gmail.com>
2025-02-28 13:21:32 +0530

    cors solved

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
+app.use(cors())
 
 // Serve static files (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
